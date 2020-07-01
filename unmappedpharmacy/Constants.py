4c1348cb52df6a0684434ca9764410a9b001f4c3
Msonic1993 <58769015+Msonic1993@users.noreply.github.com>
2020-07-01 14:52:36 +0200

    Update Constants.py

--- unmappedpharmacy/Constants.py
+++ unmappedpharmacy/Constants.py
@@ -2,32 +2,12 @@ import time
 import mysql.connector
 #stałe dla xx
 
-#
-# timestr = time.strftime("%Y-%m-%d")
-# RemoteName = "Apteki.csv"
-# PfizerPartnerPlusRemoteDir = "/xx/xx-xx/xx-php/xx/xx"
-# PfizerIpraRemoteDir = "/xx/xx-common/common-etl/xx-xx/xx-etl/xx-raw"
-# ExtractFolder ="/pexxtarda/xx-common/temp/"
-# host ="xx.xx.local"
-# user ="xx"
-# password = "xx!"
-#
-# PfizerDB = mysql.connector.connect(
-# host="xx",
-# port="xx",
-# user="xx",
-# passwd="xx",
-# database="xx"
-#  )
-# aNetworkID = "621"
-# PPlusNetworkID = "587"
-
 
 #Stałe dla xxxx
-RBIpraRemoteDir = "/xx/xx-xx-xx/xx"
-RBPartnerPlusRemoteDir = "/xx/xx-xx-xx/xx"
+zmienna1 = "/xx/xx-xx-xx/xx"
+zmienna2 = "/xx/xx-xx-xx/xx"
 
-ReckittDB = mysql.connector.connect(
+RecDB = mysql.connector.connect(
     host="xx.xx.xx.xx",
     port="xx",
     user="root",
